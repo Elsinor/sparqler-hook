176b80c3b63a4ded9ed0fd462a6a69d11871d4df
Elsinor <kr3at00r@gmail.com>
2017-09-14 22:16:47 +0200

    Add files via upload

---
+++ get-food-from.py
@@ -0,0 +1,24 @@
+country = Hook['params']['country']
+print(country)
+
+import pprint
+import requests
+import urllib
+
+q = """PREFIX dbo: <http://dbpedia.org/ontology/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX dbr: <http://dbpedia.org/resource/>
+select distinct ?food
+where {
+?food rdf:type dbo:Food .
+?food dbo:country dbr:Italy .
+}
+LIMIT 100"""
+params = {"query":q}
+ash = urllib.urlencode(params)
+
+print(ash)
+
+DBpedia = "http://it.dbpedia.org/sparql?"+ash+"&format=json&run=+Run+Query+"
+r = requests.get(DBpedia)
+print(r.text)
