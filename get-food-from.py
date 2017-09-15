a49f3751619d0e5ff96b0d150c36d6ba08357541
Elsinor <kr3at00r@gmail.com>
2017-09-15 22:42:30 +0200

    Update get-food-from.py

--- get-food-from.py
+++ get-food-from.py
@@ -1,20 +1,30 @@
-country = Hook['params']['country']
-region = Hook['params']['region']
-print(country)
-print(region)
-
 import pprint
 import requests
 import urllib
+
+hasCountry = hasRegion = false;
+
+if (Hook['params']['country'] != None)
+  hasCountry = true;
+if (Hook['params']['region'] != None)
+  hasRegion = true;
+
+country = region = "";
+if (hasCountry)
+  country = "?food dbo:country dbr:" + Hook['params']['country'] + " . "
+if (hasRegion)
+  region = "?food dbo:region dbr:" + Hook['params']['region'] + " . " 
+  
+print(country)
+print(region)
+
 
 q = """PREFIX dbo: <http://dbpedia.org/ontology/>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX dbr: <http://dbpedia.org/resource/>
 select distinct ?food
 where {
-?food rdf:type dbo:Food .
-?food dbo:country dbr:Italy .
-}
+?food rdf:type dbo:Food . """ + country + region + """}
 LIMIT 100"""
 params = {"query":q}
 ash = urllib.urlencode(params)
