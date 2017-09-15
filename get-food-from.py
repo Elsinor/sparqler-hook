561e15460916b37012fe766e6bbdc6e9006da2f1
Gerbinix <gabriele.gerbino@live.it>
2017-09-16 00:51:14 +0200

    Update get-food-from.py

--- get-food-from.py
+++ get-food-from.py
@@ -26,7 +26,7 @@ print(region)
 q = """PREFIX dbo: <http://dbpedia.org/ontology/>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX dbr: <http://dbpedia.org/resource/>
-select distinct ?food ?thumbnail ?name
+select distinct ?food ?thumbnail
 where {
 ?food rdf:type dbo:Food . """ + country + region + """
   OPTIONAL {
