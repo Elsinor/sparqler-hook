2a95de149e063a707785e711e43564a77348a744
Gerbinix <gabriele.gerbino@live.it>
2017-09-17 17:32:39 +0200

    Update get-food-from.py

--- get-food-from.py
+++ get-food-from.py
@@ -20,7 +20,7 @@ if hasRegion:
 q = """PREFIX dbo: <http://dbpedia.org/ontology/>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX dbr: <http://dbpedia.org/resource/>
-select distinct ?food ?thumbnail ?label
+select distinct ?food ?thumbnail ?label ?abstract 
 where {
 ?food rdf:type dbo:Food . """ + country + region + """
   OPTIONAL {
