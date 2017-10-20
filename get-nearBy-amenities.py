2f8968e94b5b7881624b466ba12ab1402eb8dd1a
Elsinor <kr3at00r@gmail.com>
2017-10-20 11:38:37 +0200

    Update get-nearBy-amenities.py

--- get-nearBy-amenities.py
+++ get-nearBy-amenities.py
@@ -4,40 +4,38 @@ import requests
 import urllib
 import json
 
-x=1
-if x==1:
-    lng = "38.117665"
-    print('lng: ' + lng + '<br>')
-    if x==1:
-        lat = "13.3678464"
-        print('lat: ' + lat + '<br>')
-        q = """PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+lng = "38.117665"
+print('lng: ' + lng + '<br>')
 
-        PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
-        Prefix lgdo: <http://linkedgeodata.org/ontology/>
+lat = "13.3678464"
+print('lat: ' + lat + '<br>')
+q = """PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
-        Select *
-        From <http://linkedgeodata.org> {
-        ?uri
-            a lgdo:Amenity ;
-            rdfs:label ?name ;    
-            geo:lat ?lat ;
-            geo:long ?long .
+PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+Prefix lgdo: <http://linkedgeodata.org/ontology/>
 
-            Filter(bif:st_intersects (bif:st_point (?long, ?lat), bif:st_point (""" + lng + ", " + lat+ """), 0.1)) .
-        }
-        """
-  
-        params = {"query":q}
-        ash = urllib.urlencode(params)
+Select *
+From <http://linkedgeodata.org> {
+?uri
+    a lgdo:Amenity ;
+    rdfs:label ?name ;    
+    geo:lat ?lat ;
+    geo:long ?long .
 
-        LinkedGeoData = "http://linkedgeodata.org/sparql?"+ash+"&format=json&run=+Run+Query+"
+    Filter(bif:st_intersects (bif:st_point (?long, ?lat), bif:st_point (""" + lng + ", " + lat+ """), 0.1)) .
+}
+"""
 
-        #DBlink = DBpedia['value']
+params = {"query":q}
+ash = urllib.urlencode(params)
+
+LinkedGeoData = "http://linkedgeodata.org/sparql?"+ash+"&format=json&run=+Run+Query+"
+
+#DBlink = DBpedia['value']
 
 
-        r = requests.get(LinkedGeoData)
-        results = json.loads(r.text)
+r = requests.get(LinkedGeoData)
+results = json.loads(r.text)
 
-        print (results)
+print (results)
        
