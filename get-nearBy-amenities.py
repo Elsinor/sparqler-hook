be84c1d467825a8ea2e3ab605846a0bea62730cf
Gerbinix <gabriele.gerbino@live.it>
2017-10-20 11:36:53 +0200

    Update get-nearBy-amenities.py

--- get-nearBy-amenities.py
+++ get-nearBy-amenities.py
@@ -1,14 +1,15 @@
+print('lol')
 import pprint
 import requests
 import urllib
 import json
 
-print('lol')
-if Hook['params'].has_key('lng'):
-    lng = Hook['params']['lng']
+x=1
+if x==1:
+    lng = "38.117665"
     print('lng: ' + lng + '<br>')
-    if Hook['params'].has_key('lat'):
-        lat = Hook['params']['lat']
+    if x==1:
+        lat = "13.3678464"
         print('lat: ' + lat + '<br>')
         q = """PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
@@ -39,34 +40,4 @@ if Hook['params'].has_key('lng'):
         results = json.loads(r.text)
 
         print (results)
-        amenities = {}
-
-        for result in results["results"]["bindings"]:
-        
-            if result['church'].has_key('value'):
-                resource = result['church']['value']
-
-                if result.has_key('label'):
-                name = result['label']['value']
-                lang = result['label']['xml:lang']      
-                    
-                if amenities.has_key(resource):
-                    amenities[resource]['name'][lang] = name
-                else:
-                    amenities[resource] = {}
-                    amenities[resource]['name'] = {}
-                    amenities[resource]['name'][lang] = name
-                    
-                    lat = result['lat']['value']
-                    amenities[resource]['lat'] = lat
-                    long = result['long']['value']
-                    amenities[resource]['long'] = long
-                
-                    if result.has_key('thumbnail'):
-                    #church['img'] = result["thumbnail"]["value"]
-                    img = result['thumbnail']['value']
-                    amenities[resource]['img'] = img
-
-        amenities = json.dumps(amenities)
-        #print(r.text)
-        #print(amenities)
+       
