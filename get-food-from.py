11441b3bab0336347775f58601616695dd4cfc87
Elsinor <kr3at00r@gmail.com>
2017-10-20 11:32:28 +0200

    Update get-food-from.py

--- get-food-from.py
+++ get-food-from.py
@@ -3,81 +3,70 @@ import requests
 import urllib
 import json
 
+print('lol')
+if Hook['params'].has_key('lng'):
+    lng = Hook['params']['lng']
+    print('lng: ' + lng + '<br>')
+    if Hook['params'].has_key('lat'):
+        lat = Hook['params']['lat']
+        print('lat: ' + lat + '<br>')
+        q = """PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
-hasCountry = hasRegion = False
+        PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+        Prefix lgdo: <http://linkedgeodata.org/ontology/>
 
-if Hook['params'].has_key('country'):
-  hasCountry = True
-if Hook['params'].has_key('region'):
-  hasRegion = True
+        Select *
+        From <http://linkedgeodata.org> {
+        ?uri
+            a lgdo:Amenity ;
+            rdfs:label ?name ;    
+            geo:lat ?lat ;
+            geo:long ?long .
 
-country = region = ""
-if hasCountry:
-  country = "?food dbo:country dbr:" + Hook['params']['country'] + " . "
-if hasRegion:
-  region = "?food dbo:region dbr:" + Hook['params']['region'] + " . " 
+            Filter(bif:st_intersects (bif:st_point (?long, ?lat), bif:st_point (""" + lng + ", " + lat+ """), 0.1)) .
+        }
+        """
   
-q = """PREFIX dbo: <http://dbpedia.org/ontology/>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX dbr: <http://dbpedia.org/resource/>
+        params = {"query":q}
+        ash = urllib.urlencode(params)
 
-select distinct ?food ?thumbnail ?label ?abstract 
-where {
-?food rdf:type dbo:Food . """ + country + region + """
-  OPTIONAL {
-           ?food dbo:abstract ?abstract .
-           }
-  OPTIONAL{
-           ?food dbo:thumbnail ?thumbnail .
-           }
-  OPTIONAL{
-           ?food rdfs:label ?label .
-       }
-}
-"""
-          
-  
-params = {"query":q}
-ash = urllib.urlencode(params)
+        LinkedGeoData = "http://linkedgeodata.org/sparql?"+ash+"&format=json&run=+Run+Query+"
 
-DBpedia = "http://dbpedia.org/sparql?"+ash+"&format=json&run=+Run+Query+"
-
-#DBlink = DBpedia['value']
+        #DBlink = DBpedia['value']
 
 
-r = requests.get(DBpedia)
-results = json.loads(r.text)
+        r = requests.get(LinkedGeoData)
+        results = json.loads(r.text)
 
-foods = {}
+        print (results)
+        amenities = {}
 
-for result in results["results"]["bindings"]:
-  
-  if result['food'].has_key('value'):
-    resource = result['food']['value']
-    
-    desc = ''
-    if result.has_key('label'):
-      name = result['label']['value']
-      lang = result['label']['xml:lang']      
-      
-      if result.has_key('abstract'):
-          desc = result['abstract']['value']
-          
-      if foods.has_key(resource):
-        foods[resource]['name'][lang] = name
-        foods[resource]['desc'][lang] = desc
-      else:
-        foods[resource] = {}
-        foods[resource]['name'] = {}
-        foods[resource]['desc'] = {}
-        foods[resource]['name'][lang] = name
-        foods[resource]['desc'][lang] = desc
-          
-        if result.has_key('thumbnail'):
-          #food['img'] = result["thumbnail"]["value"]
-          img = result['thumbnail']['value']
-          foods[resource]['img'] = img
+        for result in results["results"]["bindings"]:
+        
+        if result['church'].has_key('value'):
+            resource = result['church']['value']
 
-foods = json.dumps(foods)
-#print(r.text)
-print(foods)
+            if result.has_key('label'):
+            name = result['label']['value']
+            lang = result['label']['xml:lang']      
+                
+            if amenities.has_key(resource):
+                amenities[resource]['name'][lang] = name
+            else:
+                amenities[resource] = {}
+                amenities[resource]['name'] = {}
+                amenities[resource]['name'][lang] = name
+                
+                lat = result['lat']['value']
+                amenities[resource]['lat'] = lat
+                long = result['long']['value']
+                amenities[resource]['long'] = long
+            
+                if result.has_key('thumbnail'):
+                #church['img'] = result["thumbnail"]["value"]
+                img = result['thumbnail']['value']
+                amenities[resource]['img'] = img
+
+        amenities = json.dumps(amenities)
+        #print(r.text)
+        #print(amenities)
