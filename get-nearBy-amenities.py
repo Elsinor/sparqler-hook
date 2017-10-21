5ea573c524d07f629042197160733a4a0ff04661
Gerbinix <gabriele.gerbino@live.it>
2017-10-21 13:11:57 +0200

    Update get-nearBy-amenities.py

--- get-nearBy-amenities.py
+++ get-nearBy-amenities.py
@@ -1,41 +1,39 @@
-print('lol')
 import pprint
 import requests
 import urllib
 import json
 
-lng = "38.117665"
-print('lng: ' + lng + '<br>')
+if Hook['params'].has_key('lng'):
+    lng = Hook['params']['lng']
+    if Hook['params'].has_key('lat'):
+        lat = Hook['params']['lat']
+        q = """PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
-lat = "13.3678464"
-print('lat: ' + lat + '<br>')
-q = """PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+        Prefix lgdo: <http://linkedgeodata.org/ontology/>
 
-PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
-Prefix lgdo: <http://linkedgeodata.org/ontology/>
+        Select *
+        From <http://linkedgeodata.org> {
+        ?uri
+            a lgdo:Amenity ;
+            rdfs:label ?name ;    
+            geo:lat ?lat ;
+            geo:long ?long .
 
-Select *
-From <http://linkedgeodata.org> {
-?uri
-    a lgdo:Amenity ;
-    rdfs:label ?name ;    
-    geo:lat ?lat ;
-    geo:long ?long .
+            Filter(bif:st_intersects (bif:st_point (?long, ?lat), bif:st_point (""" + lng + ", " + lat+ """), 0.1)) .
+        }
+        """
 
-    Filter(bif:st_intersects (bif:st_point (?long, ?lat), bif:st_point (""" + lng + ", " + lat+ """), 0.1)) .
-}
-"""
+        params = {"query":q}
+        ash = urllib.urlencode(params)
 
-params = {"query":q}
-ash = urllib.urlencode(params)
+        LinkedGeoData = "http://linkedgeodata.org/sparql?"+ash+"&format=json&run=+Run+Query+"
 
-LinkedGeoData = "http://linkedgeodata.org/sparql?"+ash+"&format=json&run=+Run+Query+"
-
-#DBlink = DBpedia['value']
+        #DBlink = DBpedia['value']
 
 
-r = requests.get(LinkedGeoData)
-results = json.loads(r.text)
+        r = requests.get(LinkedGeoData)
+        results = json.loads(r.text)
 
-print (results)
-       
+        print (results)
+
